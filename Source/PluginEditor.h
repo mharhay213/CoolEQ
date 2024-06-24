8ddd7ac380d5d6f601659e83f10441033fe4fab9
mharhay213 <151762557+mharhay213@users.noreply.github.com>
2024-06-24 11:41:50 -0400

    Connected slider functionality to the GUI sliders.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -36,6 +36,12 @@ private:
     NewProjectAudioProcessor& audioProcessor;
     
     CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment, lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
+    
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
