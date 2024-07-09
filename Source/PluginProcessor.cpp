85eb60e8be1610c515acbd1236ab49c5b5a5e1cf
mharhay213 <151762557+mharhay213@users.noreply.github.com>
2024-07-09 14:46:05 -0400

    Adjusted skew factor on frequency knobs.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -270,19 +270,19 @@ juce::AudioProcessorValueTreeState::ParameterLayout
         // Add LowCut
         layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("LowCut Freq", 1), 
                                                                "LowCut Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.35f),
                                                                20.f));
         
         // Add HighCut
         layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("HighCut Freq", 1), 
                                                                "HighCut Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.35f),
                                                                20000.f));
         
         // Add Peak
         layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("Peak Freq", 1), 
                                                                "Peak Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.35f),
                                                                750.f));
         
         layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("Peak Gain", 1), 
